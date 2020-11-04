ccc6c3c2c9cc3965be433c4474849c80a03c1cb8
Mario Olivares <mario.olivares@datys.cu>
2020-11-04 13:41:54 -0500

    [ADD] Updating y and reading from Store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SessionQuery } from './state/session.query';
+import {SessionStore} from './state/session.store';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'akita';
+  myStore = new SessionStore();
+  myQuery = new SessionQuery(this.myStore);
+
+  // Estado completo
+  fullState$ = this.myQuery.select();
+
+  // Solo la propiedad token
+  token$ = this.myQuery.select(state => state.token);
+
+  // Propiedad computada
+  isLoggedIn$ = this.myQuery.select(state => state.token !== '');
+  ngOnInit(): void {
+    this.myStore.update({
+      name: this.title
+    });
+  }
 }
+
